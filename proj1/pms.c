621a9e92ae04fb8e99613838c09f5e26fac753d5
dnosko <dasa.noskova@gmail.com>
2021-03-24 11:35:45 +0100

    signed only

--- proj1/pms.c
+++ proj1/pms.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
-#include <mpi/mpi.h>
+#include <mpi.h>
 
 int main(int argc, char** argv) {
+
+    int size, procs_id;
+    static const size_t BufferSize = 16;
+    unsigned char buffer[BufferSize];
+    FILE *fp;
+
+    // read file
+    fp = fopen("../numbers","rb");
+    if (!fp) {
+        perror("fopen");
+        return -1;
+    }
+    fread(buffer, sizeof(unsigned char),BufferSize,fp);
+    for(int i = 0; i<BufferSize; i++)
+        printf("%d ", buffer[i]);
 
     MPI_Init(NULL, NULL);
 
     // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    // Get the id of the process
+    MPI_Comm_rank(MPI_COMM_WORLD, &procs_id);
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
 
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
+    /* first processor will write unordered sequence to output
+     * sorted sequence is written one by one in each line in ascending order
+     */
 
+    fclose(fp);
     // Finalize the MPI environment.
     MPI_Finalize();
 
